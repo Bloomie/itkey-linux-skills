72a623ce368433f9bb162de2429dc962d9f3a117
inkf <filatesc@gmail.com>
2015-05-29 10:59:37 +0300

    scripts updated for threading

--- fill_es.py
+++ fill_es.py
@@ -1,15 +1,20 @@
 import elasticsearch
 
+from time import sleep
+from threading import Thread
+
 import generate_docs
 
-CHUNK_SIZE = 2500
+CHUNK_SIZE = 1250
 DOC_NUMBER = 1000
 
 INDEX_NAME = 'test'
 TYPE_NAME = 'document'
 
+search_data = []
+
 def run(start_index):
-	es = elasticsearch.Elasticsearch([{'host': 'localhost', 'port': 9200}])
+	es = elasticsearch.Elasticsearch([{'host': '10.2.55.145', 'port': 9200}])
 	for chunk in xrange(0, CHUNK_SIZE):
 		bulk_data = []
 		for i in xrange(0, DOC_NUMBER):
@@ -23,19 +28,31 @@ def run(start_index):
 			doc = generate_docs.generate_doc()
 			bulk_data.append(op_dict)
 			bulk_data.append(doc)
-		es.bulk(index=INDEX_NAME, body=bulk_data, refresh=True)
+		try:
+			es.bulk(index=INDEX_NAME, body=bulk_data, refresh=True)
+		except:
+			sleep(20)
+			print "sleeping"
+			es.bulk(index=INDEX_NAME, body=bulk_data, refresh=True)
+		print chunk
+		search_data.append(bulk_data[0])
 
 
 threads = []
 
 for i in xrange(0, 4):
-        t = Thread(target=run, args=(i * 2500000,))
-        threads.append(t)
-        t.start()
+    t = Thread(target=run, args=(i * 2500000,))
+    threads.append(t)
+    t.start()
 
 
 for i in xrange(0, 4):
-        threads[i].join()
+    threads[i].join()
+
+
+with open("search_data", "w") as f:
+    for item in search_data:
+        f.write('%s\n' % item)
 
 
 
--- fill_mongo.py
+++ fill_mongo.py
@@ -1,22 +1,47 @@
 import pymongo
 from pymongo import MongoClient
+import os
+from threading import Thread
+from time import sleep
 
 import generate_docs
 
 client = MongoClient('localhost', 27017)
-db = client['test']
+db = client['testdb']
 
 DOC_NUMBER = 1000
-CHUNK_SIZE = 10000
+CHUNK_SIZE = 625
 
-for chunk in xrange CHUNK_SIZE:
-	posts = []
-	for i in xrange(0, DOC_NUMBER):
-		doc = generate_docs.generate_doc()
-		doc['_id'] = i
-		posts.append(doc)
-		db.testcol.insert_many(posts)
+START_POINT = 0
 
-# print 'searching', doc['name']
-# for post in db.testcol.find({"name": doc['name']}):
-# 	print post
+search_data = []
+
+def run(start_index):
+    for chunk in xrange(0, CHUNK_SIZE):
+        posts = []
+        for i in xrange(0, DOC_NUMBER):
+                doc = generate_docs.generate_doc()
+                doc['_id'] = start_index + i + chunk * 1000
+                posts.append(doc)
+        db.testcol.insert_many(posts)
+        print chunk
+        search_data.append(posts[0])
+
+
+processes = []
+
+threads = []
+
+for i in xrange(0, 4):
+        t = Thread(target=run, args=(i * 2500000 + START_POINT,))
+        threads.append(t)
+        t.start()
+
+
+for i in xrange(0, 4):
+        threads[i].join()
+
+
+with open("mongo_search_data", "w") as f:
+    for item in search_data:
+        f.write('%s\n' % item)
